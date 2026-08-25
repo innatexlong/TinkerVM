ce9b03bd529166463dd357c2c1d78fb5e80bed8c
innatexlong <innxlong@outlook.com>
2026-08-25 10:14:13 +0800

    0.6.0-alpha.8
    Add REPL

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,43 @@ pub mod value;
 pub mod env;
 pub mod parser;
 
-pub fn repl() -> () { todo!() }  // TODO
+pub fn repl() -> () {
+    use std::io::Read;
+    let memory_pool = memory::MemoryPool::new(0, 1024);
+    let env = env::Env::new(std::sync::Arc::new(std::sync::RwLock::new(memory_pool)), None);
+    let env_arc = std::sync::Arc::new(std::sync::RwLock::new(env));
+    println!("TinkerVM REPL");
+    println!("After entering the complete codes, please press Ctrl+Z (Ctrl+D on Unix/Linux)");
+    println!("-----------------------------------------------------------------------------");
+    loop {
+        use std::io::Write;
+        print!(">>> ");
+        std::io::stdout().flush().unwrap();
+        let mut stdin = std::io::stdin();
+        let mut env_arc_guard = env_arc.write().unwrap();
+        let mut cursor = parser::asm::CursorPos::new();
+        let mut input_vec = Vec::new();
+        if stdin.read_to_end(&mut input_vec).unwrap() == 0 {
+            break;
+        }
+        let mut input_reader = std::io::BufReader::new(std::io::Cursor::new(input_vec));
+
+        if let Err(err) = parser::asm::compile_assembly(&mut input_reader, &mut env_arc_guard, &mut cursor) {
+            println!("{:?}", err);
+            println!("---------------");
+        } else {
+            drop(env_arc_guard);
+
+            println!("------RUN------");
+
+            match parser::exec::run(env_arc.clone()) {
+                Err(err) => println!("{:?}", err),
+                Ok(res) => println!("{res}"),
+            }
+            println!("------END------");
+        }
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
@@ -12,8 +48,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let filename = match args.get(1) {
         Some(file) => file.clone(),
         None => {
-            println!("No files specified, using 'example.tas'");
-            "example.tas".to_string()
+            repl();
+            return Ok(())
+            // println!("No files specified, using 'example.tas'");
+            // "example.tas".to_string()
         }
     };
     let file = match std::fs::File::open(&filename) {
